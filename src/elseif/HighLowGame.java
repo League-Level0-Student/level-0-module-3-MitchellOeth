25313cc87916c4fbacd8832b978cbbc0d040c33e
league <league@wts13.attlocal.net>
2018-01-14 11:31:12 -0800

    highLow

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -15,19 +15,33 @@ public class HighLowGame {
 		// 2. Print out the random variable above
 	System.out.println(random);
 		// 11. do the following 10 times
+		for (int t = 0; t < 10; t++) {
+			
 		
 			// 1. ask the user for a guess using a pop-up window, and save their response 
-String answer = JOptionPane.showInputDialog(null, "Whats your guess?");
+String answer = JOptionPane.showInputDialog(null, "I have a number from 1-100. Can you guess what it is?");
 			// 4. convert the users’ answer to an int (Integer.parseInt(string))
-Integer.parseInt(random);
+int i = Integer.parseInt(answer);
 			// 5. if the guess is correct
+if (i==random) {
+	JOptionPane.showMessageDialog(null, "You guessed correct!");
+	System.exit(0);
+}
 				// 6. win
 			// 7. if the guess is high
+if (i>random) {
+	JOptionPane.showMessageDialog(null, "Your guess is too high! Guess again.");
+}
 				// 8. tell them it's too high
 			// 9. if the guess is low
 				// 10. tell them it's too low
-
+if (i<random) {
+	JOptionPane.showMessageDialog(null, "Your guess is too low! Guess again.");
+}
+		}
 		// 12. tell them they lose
+JOptionPane.showMessageDialog(null, "You lose!");
+System.exit(0);
 	}
 
 }
